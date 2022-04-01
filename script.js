9e274595abddd38997e68d2e013107aa1bb86cb9
ex1jame <81546043+ex1jame@users.noreply.github.com>
2022-04-01 16:19:12 +0600

    console

---
+++ script.js
@@ -0,0 +1,26 @@
+const a = "Vika"
+const b = "She’s got brown hair."
+const c = "Her eyes are blue."
+
+console.log(" I’d like to tell you about my friend. " +
+    " My friend’s name is " + a +
+    " She is of my age. " +
+    b + c +
+    " She’s slim and not very tall." +
+    " We’ve been friends for a couple of years. " +
+    " She’s a very nice person, that’s why I like to spend time with her. " +
+    " We’ve got much in common, so we often have topics to discuss. " +
+    " We can chat about everything: films, music, talk-shows, books, celebrities. ")
+
+const js =
+    '   *  *** \n' +
+    '   *  *   \n' +
+    '   *  *** \n' +
+    '*  *    * \n' +
+    ' * *  *** '
+
+console.log(
+    js
+)
+
+
